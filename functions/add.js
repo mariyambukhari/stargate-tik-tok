a735a3a0de5c589216966f24bd952361bcac0ef1
Mariyam R.B <91658659+mariyambukhari@users.noreply.github.com>
2021-11-27 00:04:36 +0500

    Update add.js
    user id and password has been replaced with application token that one has to generate from the astra db website.
    the .env file will be modified accordingly. it will have ID,REGION,APPLICATIONTOKEN and KEYSPACE

--- functions/add.js
+++ functions/add.js
@@ -7,8 +7,7 @@ exports.handler = async function (event, context, callback) {
   const astraClient = await createClient({
     astraDatabaseId: process.env.ASTRA_DB_ID,
     astraDatabaseRegion: process.env.ASTRA_DB_REGION,
-    username: process.env.ASTRA_DB_USERNAME,
-    password: process.env.ASTRA_DB_PASSWORD,
+    applicationToken: process.env.ASTRA_DB_APPLICATION_TOKEN,
   });
 
   const users = astraClient
